aa755d75c5b40f5b3648b845c020874ca42429af
grzegorzSpiewak <grzegorz.spiewak1986@gmail.com>
2016-12-20 23:24:00 +0100

    Drum machine solutions

---
+++ 1. Drum machine/js/main.js
@@ -0,0 +1,45 @@
+/*===== Player Function =====*/
+const playKeys = {
+  config: {
+    $keys: document.querySelectorAll('.key'),
+    $window: window
+  },
+
+  playAudio: function (event) {
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    if(!audio) return;  // Disablle error when other keys pressed
+    audio.currentTime = 0; // Shortens the time of played audio
+    audio.play();
+  },
+
+  removeTransition: function (event) {    // Removes animation
+    if (event.propertyName !== 'transform') return;
+    event.target.classList.remove('playing');
+  },
+
+  transitionListener: function(event) { // Listenes to "changes on keys" and apply remove function
+    const keys = this.config.$keys;
+    keys.forEach(key => key.addEventListener('transitionend', this.removeTransition.bind(this)));
+  },
+
+  keyAnimation: function (event) {
+    const key = document.querySelector(`div[data-key="${event.keyCode}"]`);
+    key.classList.add('playing');
+    this.transitionListener();
+  },
+
+  audioAndAnimation: function(event) { //One to rule them all
+    this.keyAnimation(event);
+    this.playAudio(event);
+  },
+
+  eventHandler: function (event) { 
+    this.config.$window.addEventListener('keydown', this.audioAndAnimation.bind(this));
+  },
+
+  initialize: function() {
+    this.eventHandler();
+  }
+};
+
+playKeys.initialize();
---
+++ README.md
@@ -0,0 +1,3 @@
+# 30-Pure-JavaScript-challenges
+
+My solutions for 30 Day Vanilla JS Challenge
